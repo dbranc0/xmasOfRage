7f07833228e2b6da729e7a503482b3ae7d3b104a
Davide Branco <davidegabrielbranco@hotmail.com>
2017-11-29 20:17:39 -0100

    Menu scrolling infinite working

--- src/org/academiadecodigo/bootcamp/Menu.java
+++ src/org/academiadecodigo/bootcamp/Menu.java
@@ -21,29 +21,29 @@ public class Menu implements KeyboardHandler{
     public Menu(){
         //MUDAR ISTO DEPOIS
         this.menu=new Picture(0,UploadImages.PADDING,"background/java.png");
-        this.menu1=new Picture(0,UploadImages.PADDING,"background/java1.png");
+        this.menu1=new Picture(menu.getMaxX(),UploadImages.PADDING,"background/java1.png");
 
         menu.draw();
-
+        menu1.draw();
+        System.out.println("Menu: " + menu.getX() + " - " + menu.getMaxX());
+        System.out.println("Menu1: " + menu1.getX() + " - " + menu1.getMaxX());
 
     }
 
     public void imageRoll(){
-        if(menu.getMaxX()< 810 && menu.getX()>-960 ){
-            this.menu1=new Picture(menu.getMaxX(),UploadImages.PADDING,"background/java1.png");
-            menu1.draw();
+        if(menu.getX() < 810 && menu.getMaxX() > 0){
+            if(menu1.getMaxX() == 0){menu1.translate(960*2 - 10,0);}
             menu.translate(-10,0);
             menu1.translate(-10,0);
-            System.out.println(menu1.getX());
-        }else if(menu1.getMaxX()<10){
-            this.menu=new Picture(menu1.getMaxX(),UploadImages.PADDING,"background/java.png");
-            menu.draw();
-            menu.translate(-10,0);
+        }else if(menu1.getX() < 810 && menu1.getMaxX() > 0){
+            if(menu.getMaxX() == 0){menu.translate(960*2 - 10,0);}
             menu1.translate(-10,0);
-
-            }else{
             menu.translate(-10,0);
         }
+
+        System.out.println("Menu: " + menu.getX() + " - " + menu.getMaxX());
+        System.out.println("Menu1: " + menu1.getX() + " - " + menu1.getMaxX());
+
     }
 
 
